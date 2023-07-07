adaf26a583b85a67bc40f3b249f1fa8b8b29c9fb
Warren Ehrenfried <56315898+warrenehrenfried@users.noreply.github.com>
2023-07-06 23:08:44 -0400

    Updated app for data visualization

--- RideshareProject/AustinRideshare/app.R
+++ RideshareProject/AustinRideshare/app.R
@@ -12,6 +12,10 @@ library(leaflet)
 library(tidyverse)
 library(leaflet.extras) 
 library(shinydashboard)
+library(rgdal)
+library(sp)
+library(sf)
+library(plotly)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -22,39 +26,92 @@ ui <- fluidPage(
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
+            selectizeInput('dayInput', 'Select days of week:',
+                           choices = unique(rideshare$day),
+                           multiple = T,
+                           selected = unique(rideshare$day)),
+            
+            sliderInput('monthInput', 'Select months:',
                         min = 1,
-                        max = 50,
-                        value = 30)
+                        max = 12,
+                        value = c(1, 12)),
+            
+            sliderInput('hourInput', 'Select hours of day:',
+                        min = 0,
+                        max = 23,
+                        value = c(0, 23)),
+            
+            selectInput('coordLocation', 'Select coordinates:', 
+                        choices = c('Pick-Up', 'Drop-Off')),
+            
+            downloadButton('downLoad', 'Download Filtered Dataset')
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           leafletOutput("dataMap", height = '80vh')
+            tabsetPanel(type = 'tabs',
+                        tabPanel('Heatmap', leafletOutput('dataMap', height = '80vh')),
+                        tabPanel('Daily', plotlyOutput('dayBar'), height = '80vh'))
+            )
         )
     )
-)
+
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
     
-    ride_austin <- reactive({
+    filtered_data <- reactive({
         
-        df 
+        rideshare %>%
+            filter(day %in% input$dayInput) %>%
+            filter(hour %in% input$hourInput) %>%
+            filter(month %in% input$monthInput)
+    })
+    
+    data_by_day <- reactive({
+        
+        filtered_data() %>%
+            group_by(day) %>%
+            summarise(total = n())
+    })
+    
+    coordinates <- reactive({
+        # Decide which set of coordinates to use based on user input
+        if (input$coordLocation == "Pick Up") {
+            return(list(lng = filtered_data()$start_location_long, 
+                        lat = filtered_data()$start_location_lat))
+        } else {
+            return(list(lng = filtered_data()$end_location_long,
+                        lat = filtered_data()$end_location_lat))
+        }
     })
     
     output$dataMap <- renderLeaflet({
         
-        # Use leaflet to make map using data filtered with filt_chem variable above
-        leaflet(data = ride_austin()) %>%
-            # Use generic basemap 'tiles'
+        leaflet(data = filtered_data()) %>%
             addTiles() %>%
-            addHeatmap(lng = ~end_location_long, lat = ~end_location_lat,
+            # addMarkers(data = bus_stops,
+            #            lng = bus_stops$LONGITUDE,
+            #            lat = bus_stops$LATITUDE) %>%
+            addHeatmap(lng = coordinates()$lng, 
+                       lat = coordinates()$lat,
                        blur = 25,
-                       max = 200,
-                       radius = 50)
+                       max = nrow(filtered_data()) / (nrow(filtered_data()) * .01),
+                       radius = 30) 
     })
+    
+    output$dayBar <- renderPlotly({
+        ggplot(data = data_by_day(), aes(x = day, y = total, group = day)) +
+            geom_bar(stat = 'identity') +
+            ggtitle('Daily Ride Totals')
+    })
+    
+    output$downLoad <- downloadHandler(
+        filename = 'filtered_dataset.csv',
+        content = function(file) {
+            write.csv(filtered_data(), file)
+        }
+    )
 }
 
 # Run the application 
