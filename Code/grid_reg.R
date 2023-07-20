49b7ba56d1502c4101c1c91de18664f91f8dbb21
sittingman <sitkevin@gmail.com>
2023-07-19 21:42:14 -0700

    Redo regerssion by grid and include stat test

--- Code/grid_reg.R
+++ Code/grid_reg.R
@@ -7,14 +7,11 @@ library(tidyverse)
 library(lubridate)
 library(fuzzyjoin)
 
-grid_map <- read.csv("../Data/master_data_file.csv")
-grid <- unique(grid_map[, c("start_location_lat", "start_location_long",
-  "start_grid_cell")])
-
-
 # Ride share data processing
 
 df <- read.csv("../Data/RideAustin_Weather.csv")
+
+summary(df)
 rideshare <- df %>%
   filter(distance_travelled < 15000) %>%
   mutate(dist_km = distance_travelled/1000, dow = wday(ymd(Date),
@@ -31,41 +28,120 @@ rideshare$hour <- rideshare$started_on %>%
   as.POSIXct(tz = Sys.timezone()) %>%
   hour()
 
-rideshare$wkd <- ifelse((rideshare$dow == "Sun" | rideshare$dow ==
-  "Sat"), "weekend", "weekday")
-rideshare$hr_cat <- ifelse((rideshare$hour >= 6 & rideshare$hour <
-  18), "workhour", "offpeak")
 
 rideshare_gp <- rideshare %>%
-  group_by(start_location_long, start_location_lat, wkd, hr_cat) %>%
+  group_by(dow, hour) %>%
   summarise(ride_vol = n() * 1.4)
-rideshare_gp$ride_vol_norm <- ifelse(rideshare_gp$wkd == "weekend",
-  rideshare_gp$ride_vol/2, rideshare_gp$ride_vol/5)
-rideshare_final <- merge(rideshare_gp, grid, by = c("start_location_long",
-  "start_location_lat"))
 
+
+rideshare_gp$wkd_end <- ifelse((rideshare_gp$dow == "Sun" | rideshare_gp$dow ==
+  "Sat"), "weekend", "weekday")
+rideshare_gp$hr_cat <- ifelse((rideshare_gp$hour < 6), "darkout",
+  ifelse((rideshare_gp$hour >= 6 & rideshare_gp$hour < 12),
+    "rush_hour", ifelse((rideshare_gp$hour >= 12 & rideshare_gp$hour <
+      18), "work_hour", "evening")))
+
+
+summary(lm(ride_vol ~ wkd_end + hr_cat, data = rideshare_gp))
+
+# K-S Test
+fog_count <- rideshare %>%
+  group_by(Fog) %>%
+  summarise(n = n()) %>%
+  filter(Fog == 1)
+thunder_count <- rideshare %>%
+  group_by(Thunder) %>%
+  summarise(n = n()) %>%
+  filter(Thunder == 1)
+
+set.seed(123)
+no_fog <- rideshare %>%
+  filter(Fog == 0) %>%
+  sample_n(size = fog_count$n) %>%
+  group_by(dow) %>%
+  summarize(n = n())
+fog <- rideshare %>%
+  filter(Fog == 1) %>%
+  group_by(dow) %>%
+  summarize(n = n())
+ks.test(no_fog$n, fog$n)
+
+no_thunder <- rideshare %>%
+  filter(Thunder == 0) %>%
+  sample_n(size = thunder_count$n) %>%
+  group_by(dow) %>%
+  summarize(n = n())
+thunder <- rideshare %>%
+  filter(Thunder == 1) %>%
+  group_by(dow) %>%
+  summarize(n = n())
+ks.test(no_thunder$n, thunder$n)
 
 # Bus ride dataset
 
 capmetro <- read.csv("../Data/CapMetro ridership all by latlong dow hour.csv")
-capmetro$wkd <- ifelse((capmetro$start_time_dayofwk == 6 | capmetro$start_time_dayofwk ==
-  7), "weekend", "weekday")
-capmetro$hr_cat <- ifelse((capmetro$start_time_hour >= 6 & capmetro$start_time_hour <
-  18), "workhour", "offpeak")
-
+capmetro$dow <- substr(weekdays(as.Date("2017-01-01") + capmetro$start_time_dayofwk),
+  1, 3)
 capmetro_gp <- capmetro %>%
-  group_by(veh_lat_rnd, veh_long_rnd, wkd, hr_cat) %>%
+  group_by(dow, start_time_hour) %>%
   summarize(bus_vol = sum(rides_on))
-capmetro_gp$bus_vol_norm <- ifelse(capmetro_gp$wkd == "weekend",
-  capmetro_gp$bus_vol/2, capmetro_gp$bus_vol/5)
-capmetro_final <- merge(capmetro_gp, grid, by.x = c("veh_lat_rnd",
-  "veh_long_rnd"), by.y = c("start_location_lat", "start_location_long"))
+capmetro_gp$wkd_end <- ifelse((capmetro_gp$dow == "Sat" | capmetro_gp$dow ==
+  "Sun"), "weekend", "weekday")
+capmetro_gp$hr_cat <- ifelse((capmetro_gp$start_time_hour < 6),
+  "darkout", ifelse((capmetro_gp$start_time_hour >= 6 & capmetro_gp$start_time_hour <
+    12), "rush_hour", ifelse((capmetro_gp$start_time_hour >=
+    12 & capmetro_gp$start_time_hour < 18), "work_hour",
+    "evening")))
 
-final_df <- merge(rideshare_final, capmetro_final, by.x = c("start_location_long",
-  "start_location_lat", 'wkd', 'hr_cat', 'start_grid_cell'), by.y = c("veh_long_rnd", "veh_lat_rnd", 'wkd', 'hr_cat', 'start_grid_cell'))
 
-reg <- lm(ride_vol_norm ~ bus_vol_norm + wkd + hr_cat + start_grid_cell, data = final_df)
+final_df <- merge(rideshare_gp, capmetro_gp, by.x = c("wkd_end",
+  "hr_cat", "dow", "hour"), by.y = c("wkd_end", "hr_cat", "dow",
+  "start_time_hour"))
+
+cor.test(final_df$bus_vol, final_df$ride_vol)
+
+reg <- lm(ride_vol ~ bus_vol + wkd_end + hr_cat, data = final_df)
 summary(reg)
 
-spec_reg <- final_df %>% filter(start_grid_cell %in% c('3-A', '3-B'))
-summary(lm(ride_vol_norm ~ bus_vol_norm + hr_cat , data = spec_reg))
+# adding grid to both rideshare and capmetro dataset
+grid_map <- read.csv("../Data/master_data_file.csv")
+grid <- unique(grid_map[, c("start_location_lat", "start_location_long",
+                            "start_grid_cell")])
+
+# Note some trips will be dropped as they fall outside the 5x5 grid
+rideshare_1 <- merge(rideshare, grid, by = c("start_location_long",
+  "start_location_lat"))
+
+
+rideshare_1_gp <- rideshare_1 %>%
+  group_by(dow, hour, start_grid_cell) %>%
+  summarise(ride_vol = n() * 1.4)
+
+
+rideshare_1_gp$wkd_end <- ifelse((rideshare_1_gp$dow == "Sun" |
+  rideshare_1_gp$dow == "Sat"), "weekend", "weekday")
+rideshare_1_gp$hr_cat <- ifelse((rideshare_1_gp$hour < 6), "darkout",
+  ifelse((rideshare_1_gp$hour >= 6 & rideshare_1_gp$hour <
+    12), "rush_hour", ifelse((rideshare_1_gp$hour >= 12 &
+    rideshare_1_gp$hour < 18), "work_hour", "evening")))
+
+capmetro_1 <- capmetro
+capmetro_1 <- merge(capmetro, grid, by.x = c("veh_lat_rnd", "veh_long_rnd"),
+  by.y = c("start_location_lat", "start_location_long"))
+capmetro_1_gp <- capmetro_1 %>%
+  group_by(dow, start_time_hour, start_grid_cell) %>%
+  summarize(bus_vol = sum(rides_on))
+capmetro_1_gp$wkd_end <- ifelse((capmetro_1_gp$dow == "Sat" |
+  capmetro_1_gp$dow == "Sun"), "weekend", "weekday")
+capmetro_1_gp$hr_cat <- ifelse((capmetro_1_gp$start_time_hour <
+  6), "darkout", ifelse((capmetro_1_gp$start_time_hour >= 6 &
+  capmetro_1_gp$start_time_hour < 12), "rush_hour", ifelse((capmetro_1_gp$start_time_hour >=
+  12 & capmetro_1_gp$start_time_hour < 18), "work_hour", "evening")))
+final_df_1 <- merge(rideshare_1_gp, capmetro_1_gp, by.x = c("wkd_end",
+  "hr_cat", "dow", "hour", 'start_grid_cell'), by.y = c("wkd_end", "hr_cat", "dow",
+  "start_time_hour", 'start_grid_cell'))
+
+reg_1 <- lm(ride_vol ~ bus_vol + wkd_end + hr_cat + start_grid_cell, data = final_df_1)
+summary(reg_1)
+
+
